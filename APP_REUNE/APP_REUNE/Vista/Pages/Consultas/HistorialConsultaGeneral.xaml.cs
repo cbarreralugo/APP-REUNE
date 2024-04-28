3e92df966306ef8fbe9ac762a62c9796a8206673
Carlos Alberto Barrera Lugo <carlosalberto.barreralugo1230@hotmail.com>
2024-04-27 20:17:31 -0600

    add: estructura base de vistas

---
+++ APP_REUNE/APP_REUNE/Vista/Pages/Consultas/HistorialConsultaGeneral.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace APP_REUNE.Vista.Pages.Consultas
+{
+    /// <summary>
+    /// Lógica de interacción para HistorialConsultaGeneral.xaml
+    /// </summary>
+    public partial class HistorialConsultaGeneral : Page
+    {
+        public HistorialConsultaGeneral()
+        {
+            InitializeComponent();
+        }
+    }
+}
